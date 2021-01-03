ec9ae0ae2805b4f24b2de2360eb9ee479f9986d7
arvin2079 <arving2079@gmail.com>
2021-01-03 15:24:00 +0330

    python compiler lexer for java

---
+++ lexer/constants.py
@@ -0,0 +1,45 @@
+""" regex patterns """
+re_numeric_pattern = r'^[0-9]$'
+re_charonly_pattern = r'^[a-zA-Z]&'
+re_char_num_underlin_pattern = r'^\w$'
+
+""" keywords and symbols """
+S_EOF, S_Add, S_Sub, S_Mul, S_Div, S_Mod, S_Inc, S_Dec, \
+S_Eql, S_Neq, S_Lss, S_Grt, S_Leq, S_Geq, S_Neq, \
+S_And, S_Or, S_Not, \
+S_Asgn, S_AddAsgn, S_SubAsgn, S_MulAsgn, S_DivAsgn, S_ModAsgn, \
+S_True, S_False, S_null, \
+S_Abstract, S_Continue, S_For, S_New, S_Switch, S_Default, S_If, S_Else, S_Private, S_This, S_Break, S_Public, \
+S_Protected, S_Throw, S_Throws, S_Import, S_Enum, S_Return, S_Transient, S_Try, S_Catch, S_Extends, S_Final, \
+S_Interface, S_Static, S_Void, S_Class, S_Finally, S_Super, S_While, \
+S_Int, S_Double, S_Float, S_String, S_Boolean, S_Byte, S_Long, S_Short, S_Char, \
+S_Lbrace, S_Rbrace, S_Lparen, S_Rparen, S_Semi, S_Comma, S_Identifier = range(73)
+
+""" single character only symbols """
+symbols = {'{': S_Lbrace, '}': S_Rbrace, '(': S_Lparen, ')': S_Rparen, ';': S_Semi, ',': S_Comma}
+
+""" keywords """
+key_words = {'if': S_If, 'else': S_Else, 'while': S_While, 'true': S_True, 'false': S_False, 'null': S_null,
+             'abstract': S_Abstract, 'continue': S_Continue, 'for': S_For, 'new': S_New, 'switch': S_Switch,
+             'default': S_Default, 'if': S_If, 'else': S_Else, 'private': S_Private, 'this': S_This, 'break': S_Break,
+             'public': S_Public, 'protected': S_Protected, 'throw': S_Throw, 'throws': S_Throws, 'import': S_Import,
+             'enum': S_Enum, 'return': S_Return, 'transient': S_Transient, 'try': S_Try, 'catch': S_Catch,
+             'extends': S_Extends, 'final': S_Final, 'interface': S_Interface, 'static': S_Static, 'void': S_Void,
+             'class': S_Class, 'finally': S_Finally, 'super': S_Super, 'while': S_While, 'int': S_Int,
+             'double': S_Double, 'float': S_Float, 'String': S_String, 'boolean': S_Boolean, 'byte': S_Byte,
+             'long': S_Long, 'short': S_Short, 'char': S_Char}
+
+""" symbol values """
+symbol_values = ['end_of_file', 'op_add', 'op_sub', 'op_mul', 'op_div', 'op_mod', 'op_increment', 'op_decrement', 'op_equal',
+                 'op_not_equal', 'op_less', 'op_grater', 'op_less_equal', 'op_grater_equal', 'op_not_equal', 'op_and',
+                 'op_or', 'op_not', 'op_assign', 'op_add_asign', 'op_sub_assign', 'op_mul_assign', 'op_div_assign',
+                 'op_mod_assign', 'keyword_true', 'keyword_false', 'keyword_null', 'keyword_abstract',
+                 'keyword_continue', 'keyword_for', 'keyword_new', 'keyword_switch', 'keyword_default', 'keyword_if',
+                 'keyword_else', 'keyword_private', 'keyword_this', 'keyword_break', 'keyword_public',
+                 'keyword_protected', 'keyword_throw', 'keyword_thorws', 'keyword_import', 'keyword_enum',
+                 'keyword_return', 'keyword_transient', 'keyword_try', 'keyword_catch', 'keyword_extends',
+                 'keyword_final', 'keyword_interface', 'keyword_static', 'keyword_void', 'keyword_class',
+                 'keyword_finally', 'keyword_super', 'keyword_while', 'keyword_int', 'keyword_double', 'keyword_float',
+                 'keyword_string', 'keyword_boolean', 'keyword_byte', 'keyword_long', 'keyword_short', 'keyword_char',
+                 'left_brace', 'right_brace', 'left_paren', 'right_paren', 'semicolon', 'comma', 'identifier']
+
---
+++ lexer/lexer.py
@@ -0,0 +1,232 @@
+from . import constants
+
+current_ch = " "
+current_col = 0
+current_line = 1
+input_file = None
+
+
+def print_error(line, col, msg):
+    print(line, col, msg)
+    exit(1)
+
+
+def get_next_ch():
+    global current_ch, current_col, current_line
+
+    current_ch = input_file.read(1)
+    current_col += 1
+    if current_ch == '\n':
+        current_line += 1
+        current_col = 0
+    return current_ch
+
+
+def identity_or_num(err_line, err_col):
+    content = ""
+    is_floating_point = False
+
+    if current_ch.isnumeric():
+        content += current_ch
+        while True:
+            get_next_ch()
+            if current_ch == '.':
+                if not is_floating_point:
+                    content += current_ch
+                    get_next_ch()
+                else:
+                    print_error(err_line, err_col, 'error : wrong format number!')
+                    exit(1)
+                is_floating_point = True
+            if current_ch.isnumeric():
+                content += current_ch
+            elif current_ch.isspace():
+                break
+            else:
+                print_error(err_line, err_col, 'error : wrong format floatinpoint number!')
+                exit(1)
+        if len(content) == 0:
+            print_error(err_line, err_col, "format exeption in recognizing number!")
+        if is_floating_point:
+            return constants.S_Float, err_line, err_line, content
+        else:
+            return constants.S_Int, err_line, err_line, content
+
+    elif current_ch.isidentifier():
+        content += current_ch
+        while True:
+            get_next_ch()
+            if current_ch.isalpha() or current_ch == '_':
+                content += current_ch
+            elif current_ch.isspace():
+                break
+            else:
+                print_error(err_line, err_col, 'error : wrong format identifier!')
+                exit(1)
+        if len(content) == 0:
+            print_error(err_line, err_col, "format exeption in recognizing identifier!")
+        if content in constants.key_words:
+            return constants.key_words[content], err_line, err_col
+        return constants.S_Identifier, err_line, err_col, content
+    else:
+        print_error(err_line, err_col, "format exeption!")
+        exit(1)
+
+
+def div_or_comment(err_line, err_col):
+    if current_ch != '/':
+        while True:
+            if current_ch == '\n':
+                get_next_ch()
+                break
+
+    elif current_ch == '*':
+        while True:
+            get_next_ch()
+            if current_ch == '*':
+                if current_ch == '/':
+                    break
+    else:
+        return constants.S_Div, err_line, err_col
+
+
+def expect_string(err_line, err_col):
+    text = ""
+    while get_next_ch() != '"':
+        if len(current_ch) == 0:
+            print_error(err_line, err_col, "reach end of file while scanning string literal")
+        text += current_ch
+
+    get_next_ch()
+    return constants.S_String, err_line, err_col, text
+
+
+def expect_char(err_line, err_col):
+    char = get_next_ch()
+    if get_next_ch() != "'":
+        print_error(err_line, err_col, 'error : wrong format character')
+        exit(1)
+    get_next_ch()
+    return constants.S_Char, err_line, err_col, char
+
+
+def expect_follow(err_line, err_col, if_no_match_found, **expects):
+    get_next_ch()
+    for expect, symbol in expects:
+        if current_ch == expect:
+            get_next_ch()
+            return symbol, err_line, err_col
+
+    if if_no_match_found == constants.S_EOF:
+        print_error(err_line, err_col, "error: unrecognized character: (%d) '%c'" % (ord(current_ch), current_ch))
+
+    return if_no_match_found, err_line, err_col
+
+
+class Token:
+    def __init__(self, tok_key, line, col, desc):
+        self.tok_key = tok_key
+        self.line = line
+        self.col = col
+        self.desc = desc
+
+    def __str__(self):
+        value = f'{self.tok_key} - {self.line} - {self.col}'
+        if self.desc:
+            value += f' - {self.desc}'
+        return value
+
+
+def gettok():
+    while current_ch.isspace():
+        get_next_ch()
+
+    err_line = current_line
+    err_col = current_col
+
+    if len(current_ch) == 0:
+        return constants.S_EOF, err_line, err_col
+
+    elif current_ch == '+':
+        return expect_follow(err_line, err_col, constants.S_Add, {
+            '=': constants.S_AddAsgn,
+            '+': constants.S_Inc,
+        })
+    elif current_ch == '-':
+        return expect_follow(err_line, err_col, constants.S_Sub, {
+            '=': constants.S_SubAsgn,
+            '-': constants.S_Dec,
+        })
+    elif current_ch == '*':
+        return expect_follow(err_line, err_col, constants.S_Mul, {
+            '=': constants.S_MulAsgn,
+        })
+    elif current_ch == '%':
+        return expect_follow(err_line, err_col, constants.S_Mod, {
+            '=': constants.S_ModAsgn,
+        })
+    elif current_ch == '=':
+        return expect_follow(err_line, err_col, constants.S_Asgn, {
+            '=': constants.S_Eql,
+        })
+    elif current_ch == '!':
+        return expect_follow(err_line, err_col, constants.S_Not, {
+            '=': constants.S_Neq,
+        })
+    elif current_ch == '<':
+        return expect_follow(err_line, err_col, constants.S_Lss, {
+            '=': constants.S_Leq,
+        })
+    elif current_ch == '>':
+        return expect_follow(err_line, err_col, constants.S_Grt, {
+            '=': constants.S_Geq,
+        })
+    elif current_ch == '&':
+        return expect_follow(err_line, err_col, constants.S_And, {
+            '&': constants.S_And,
+        })
+    elif current_ch == '|':
+        return expect_follow(err_line, err_col, constants.S_Or, {
+            '|': constants.S_Or,
+        })
+    elif current_ch == '/':
+        return div_or_comment(err_line, err_col)
+    elif current_ch == '"':
+        return expect_string(err_line, err_col)
+    elif current_ch == '"':
+        return expect_char(err_line, err_col)
+    elif current_ch in constants.symbols:
+        sym = constants.symbols[current_ch]
+        get_next_ch()
+        return sym, err_line, err_col
+    else:
+        return identity_or_num(err_line, err_col)
+
+
+def lex(input_file_address: str):
+    global input_file
+
+    with open(input_file_address, mode='r') as file:
+        input_file = file
+        while True:
+            t = gettok()
+            token = Token(tok_key=t[0], line=t[1], col=t[2])
+
+            if token.tok_key == constants.S_Int or\
+                    token.tok_key == constants.S_Float or\
+                    token.tok_key == constants.S_Identifier or\
+                    token.tok_key == constants.S_String:
+                token.desc = t[3]
+
+            print(token)
+
+            if token.tok_key == constants.S_EOF:
+                break
+
+## TODO : write token class and return list of tokens
+## TODO : change upper gettoken method with switch case
+## TODO : complete other states of gettoken method
+## TODO : complete other methods
+
+## TODO : div or comment
+## TODO : floating point numbers
